9fda92bdc302549ac29bfb68d759f8b978fafcc1
Watanabe-Keita-wk <wkei1118kani@gmail.com>
2025-06-05 07:56:46 +0900

    その他設定ファイル

---
+++ front/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  runtimeConfig: {
+      mysqlHost: "mysql",
+      mysqlPort: 3306,
+      database: "AdSystem",
+      mysqlUser: "adUser",
+      mysqlPass: "adPass"
+  }
+})
