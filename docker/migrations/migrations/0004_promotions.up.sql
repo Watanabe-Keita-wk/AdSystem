5c21c6143cd633c553379448602c5ae13c82956e
Watanabe-Keita-wk <wkei1118kani@gmail.com>
2025-06-18 07:33:52 +0900

    プロモーション作成時にlp_url指定できるようにする

--- docker/migrations/migrations/0004_promotions.up.sql
+++ docker/migrations/migrations/0004_promotions.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE promotions (
     id SERIAL PRIMARY KEY,
     promotion_id VARCHAR(100) NOT NULL,
     promotion_name VARCHAR(100),
+    lp_url VARCHAR(255),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_flg TINYINT(1) NOT NULL DEFAULT 0
 );
