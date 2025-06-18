fd7459a526821f4a6de6d87436e55f6ad86d530e
Watanabe-Keita-wk <wkei1118kani@gmail.com>
2025-06-19 08:45:09 +0900

    初期ユーザー

--- docker/migrations/migrations/0001_users.up.sql
+++ docker/migrations/migrations/0001_users.up.sql
@@ -1,5 +1,12 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
+    user_id VARCHAR(100) NOT NULL,
+    user_pass VARCHAR(100) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users
+ (username, user_id, user_pass) 
+VALUES
+ ('admin', 'admin', 'pass');
